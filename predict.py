faad9c364cded46ba62366c52f45587d3f5f3f34
hiiadarshhhh <mishraadarsh6263@gmail.com>
2025-07-08 16:36:47 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,4 +1,10 @@
 import rasterio
 import numpy as np
 from tensorflow.keras.models
-import load model 
+import load_model
+from PIL import image
+
+def run_unet_prediction(tif_path,model_path):
+      model =  load_model(model_path)
+
+      with rasterio.open(tif_path)as src:
