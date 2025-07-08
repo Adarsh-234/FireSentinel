76a9a0740e349b61d7bd6c12706ff713df746844
hiiadarshhhh <mishraadarsh6263@gmail.com>
2025-07-08 17:09:35 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -8,3 +8,21 @@ def run_unet_prediction(tif_path,model_path):
       model =  load_model(model_path)
 
       with rasterio.open(tif_path)as src:
+            img = src.read()
+            img = np.moveaxis(img ,0 ,-1)
+
+pil_img = Image.fromarray(img.astype(np.uint8))
+
+pil_img_resized = pil_img.resize((256, 256) ,Image.BILINEAR)
+
+img_resized = np.array(pil_img_resized)
+
+img_resized = img_resized / 255.0
+
+prediction = model.predict(np.expand_dims(img_resized , axis = 0))[0]
+
+binary_mask = (prediction[:, :, 0] > 0.5).astype(np.uint8)
+
+preview = Image.fromarray(binary_mask * 255)
+
+return {"array":binary_mask , "preview": preview }
